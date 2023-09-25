d21c51d4ff0dd490761eeb304b1c5967ca5eea08
wisnasidabutar <wisnasidabutar.wisnasidabutar2002@gmail.com>
2023-09-25 17:07:16 +0700

    Latihan1

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,16 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>BIODATA</h1>";
+        echo "Nama : Wisnasaragi
+        <p>Nim  :19220319</p>
+        <p>Kelas    :19.3A.24</p>
+        <p>No. telp :081260375061</p>
+        <p>Hobi     :Bulutangkis</p>
+        <p>Alamat   :Cikarang selatan</p>
+        <p>Umur     :20</p>";
+    }
+
+}
--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,9 +1,10 @@
 <?php
-class Contoh1 extends CI_Controller
+class contoh1 extends CI_Controller
 {
- public function index()
+    public function index()
  {
- echo "<h1>Perkenalkan</h1>";
+ echo "<h1><?php
+Perkenalkan</h1>";
  echo"Nama saya Wisna Saragi";
  }
 }
