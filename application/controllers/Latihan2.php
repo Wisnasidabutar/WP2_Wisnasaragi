a913a73da5df9b94f4ce3f1001deb3779721ef76
wisnasidabutar <wisnasidabutar.wisnasidabutar2002@gmail.com>
2023-09-29 21:27:19 +0700

    mengerjakan contoh2

--- application/controllers/Latihan2.php
+++ application/controllers/Latihan2.php
@@ -4,6 +4,15 @@ class Latihan2 extends CI_Controller
     public function index()
     {
         echo "Selamat Datang..selamat belajar web programming";
+        //$this->load->view('view-Latihan2');
+
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->Model('Model_Latihan1');
+        $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+        echo "hasil penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil;
 
     }
     
