22805a0cec5315a30cf9b7f3dd2c5881453dd412
wisnasidabutar <wisnasidabutar.wisnasidabutar2002@gmail.com>
2023-09-29 22:48:55 +0700

    mengerjakan contoh3

--- application/controllers/Latihan2.php
+++ application/controllers/Latihan2.php
@@ -10,9 +10,12 @@ class Latihan2 extends CI_Controller
 
     public function penjumlahan($n1, $n2)
     {
-        $this->load->Model('Model_Latihan1');
-        $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
-        echo "hasil penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil;
+        $this->load->model('Model_latihan1');
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view_latihan1', $data);
 
     }
     
