22ad5f24edded488d4fbc3dd77e019ad3a06b4c8
wisnasidabutar <wisnasidabutar.wisnasidabutar2002@gmail.com>
2023-10-23 11:38:00 +0700

    mengerjakan tugas pertemuan 3

---
+++ pustaka-booking/application/controllers/latihan1_biodata.php
@@ -0,0 +1,18 @@
+<?php 
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Biodata Mahasiswa UBSI</h1>";
+        echo "<ul>";
+        echo "<li><p>Nama: Muhammad Naufal Saputra</p></li>";
+        echo "<li><p>Nim: 19220854</p></li>";
+        echo "<li><p>Kelas: 19.3A.24</p></li>";
+        echo "<li><p>No Hp : 081381653386</p></li>";
+        echo "<li><p>Hoby: Futsal</p></li>";
+        echo "<li><p>Alamat : Cibitung </p></li>";
+        echo "</ul>";
+        
+    }
+}
+?>
